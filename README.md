8b338195bc4f974f75bd74e949f739fa661fccbf
Arahman75 <selfarahman01825@gmail.com>
2023-11-29 10:40:02 +0600

    vercel deploy

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-"# b8a12-server-side-Arahman75" 
+## Website name : News Paper
+
+## live link: https://newspaper-3507b.web.app
+
 ## https://github.com/programming-hero-web-course1/b8a12-server-side-Arahman75
--- index.js
+++ index.js
@@ -6,7 +6,21 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+// app.use(cors());
+// app.use(express.json());
+// middleware
+const corsConfig = {
+    origin: [
+        'http://localhost:5173',
+        'https://newspaper-3507b.web.app',
+        'https://newspaper-3507b.firebaseapp.com',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig));
+// app.use(cors());
 app.use(express.json());
 
 
